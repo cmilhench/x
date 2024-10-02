eed3b616ccaeb56b1f98fd9f597447bc0341f319
cmilhench <cmilhench@gmail.com>
2024-10-02 15:29:27 +0100

    add additional pipeline functions

--- exp/pipeline/pipeline.go
+++ exp/pipeline/pipeline.go
@@ -5,19 +5,105 @@ package pipeline
 import (
 	"context"
 	"sync"
+	"time"
 )
 
 // Generator generates values from a slice and sends them to a channel.
 // It stops generating values when the 'done' channel is closed.
-func Generator[T any](done <-chan struct{}, in ...T) <-chan T {
+func Generator[T any](ctx context.Context, in ...T) <-chan T {
 	out := make(chan T)
 	go func() {
 		defer close(out)
 		for _, v := range in {
 			select {
 			case out <- v: // out ← %+v
-			case <-done: // canceled?
+			case <-ctx.Done(): // canceled?
 				return
+			}
+		}
+	}()
+	return out
+}
+
+// Take receives values from an input channel and sends only the first n values
+// to the output channel.
+func Take[T any](ctx context.Context, in <-chan T, n int) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		for i := 0; i < n; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- <-in: // out ← in
+			}
+		}
+	}()
+	return out
+}
+
+// Drop discards the first n values from the input channel and sends the rest
+// to the output channel.
+func Drop[T any](ctx context.Context, in <-chan T, n int) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		for i := 0; i < n; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case <-in: // in
+			}
+		}
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- v: // out ← in
+			}
+		}
+	}()
+	return out
+}
+
+// Filter applies a set of functions to values received from an input channel.
+// If all functions return true, the value is sent to the output channel.
+// The worker stops when the input channel is closed or the context is canceled.
+func Filter[T any](ctx context.Context, in <-chan T, fn ...func(T) bool) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+	outer:
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				for _, fn := range fn {
+					if !fn(v) {
+						continue outer
+					}
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
+}
+
+// Map applies a function to values received from an input channel.
+// It sends the results of the function to an output channel.
+// The function stops when the input channel is closed or the context is canceled.
+func Map[I, O any](ctx context.Context, id int, in <-chan I, fn func(context.Context, int, I) O) <-chan O {
+	out := make(chan O)
+	go func() {
+		defer close(out)
+		for i := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				out <- fn(ctx, id, i)
 			}
 		}
 	}()
@@ -55,64 +141,324 @@ func FanIn[T any](ctx context.Context, in ...<-chan T) <-chan T {
 	return out
 }
 
-func Filter[T any](ctx context.Context, in <-chan T, fn ...func(T) bool) <-chan T {
+// FanOut distributes values from the input channel to multiple worker goroutines.
+// Each worker receives and processes values using the provided function `fn`.
+func FanOut[T, O any](ctx context.Context, in <-chan T, workerCount int, fn func(context.Context, T) O) []<-chan O {
+	outs := make([]chan O, workerCount)
+	for i := range outs {
+		outs[i] = make(chan O)
+	}
+	var wg sync.WaitGroup
+	wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func(workerID int) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case v, ok := <-in:
+					if !ok {
+						return
+					}
+					o := fn(ctx, v)
+					select {
+					case outs[workerID] <- o:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(i)
+	}
+	go func() {
+		wg.Wait()
+		for _, ch := range outs {
+			close(ch)
+		}
+	}()
+	// Return the output channels (read-only) to the caller
+	result := make([]<-chan O, workerCount)
+	for i, ch := range outs {
+		result[i] = ch
+	}
+	return result
+}
+
+// Throttle limits the rate at which values are sent from the input channel to the output channel.
+// Only one value is sent every `rate` duration.
+func Throttle[T any](ctx context.Context, in <-chan T, rate time.Duration) <-chan T {
 	out := make(chan T)
 	go func() {
 		defer close(out)
-	outer:
-		for v := range in {
+		ticker := time.NewTicker(rate)
+		defer ticker.Stop()
+		for {
 			select {
 			case <-ctx.Done():
 				return
-			default:
-				for _, fn := range fn {
-					if !fn(v) {
-						continue outer
-					}
+			case v, ok := <-in:
+				if !ok {
+					return
 				}
-				out <- v
+				select {
+				case out <- v:
+					<-ticker.C
+				case <-ctx.Done():
+					return
+				}
 			}
 		}
 	}()
+
 	return out
 }
 
-// Map applies a function to values received from an input channel.
-// It sends the results of the function to an output channel.
-// The worker stops when the input channel is closed or the context is canceled.
-func Map[I, O any](ctx context.Context, id int, in <-chan I, fn func(context.Context, int, I) O) <-chan O {
-	out := make(chan O)
+// Partition splits an input channel into two based on a predicate function.
+// Values satisfying the predicate go to the first channel, others go to the second.
+func Partition[T any](ctx context.Context, id int, in <-chan T, fn func(context.Context, int, T) bool) (<-chan T, <-chan T) {
+	a := make(chan T)
+	b := make(chan T)
 	go func() {
-		defer close(out)
+		defer close(a)
+		defer close(b)
 		for i := range in {
 			select {
 			case <-ctx.Done():
 				return
 			default:
-				out <- fn(ctx, id, i)
+				if fn(ctx, id, i) {
+					a <- i
+				} else {
+					b <- i
+				}
+			}
+		}
+	}()
+	return a, b
+}
+
+// Sample sends values from the input channel to the output channel at a specified rate.
+// based upon time-based sampling. Unlike throttling (which limits the
+// frequency of event processing but may process more than one event in each interval),
+// time-based sampling captures exactly one event at the specified interval, regardless
+// of how many events pass through during that time.
+func Sample[T any](ctx context.Context, in <-chan T, rate time.Duration) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		ticker := time.NewTicker(rate)
+		defer ticker.Stop()
+		for i := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				out <- i
+			default:
 			}
 		}
 	}()
 	return out
 }
 
-func Tee[T any](ctx context.Context, in <-chan T, n int) []<-chan T {
-	chn := make([]chan T, n)
-	out := []<-chan T{}
+// Broadcast sends each value from the input channel to multiple output channels.
+func Broadcast[T any](ctx context.Context, in <-chan T, n int) []<-chan T {
+	outs := make([]chan T, n)
 	for i := 0; i < n; i++ {
-		chn[i] = make(chan T)
-		out = append(out, chn[i])
-		go func(i int) {
-			defer close(chn[i])
-			for v := range in {
-				select {
-				case <-ctx.Done():
+		outs[i] = make(chan T)
+	}
+	go func() {
+		defer func() {
+			for _, ch := range outs {
+				close(ch)
+			}
+		}()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case val, ok := <-in:
+				if !ok {
 					return
-				case chn[i] <- v:
+				}
+				var wg sync.WaitGroup
+				wg.Add(n)
+				for _, ch := range outs {
+					go func(c chan T) {
+						defer wg.Done()
+						select {
+						case c <- val:
+						case <-ctx.Done():
+						}
+					}(ch)
+				}
+
+				wg.Wait()
+			}
+		}
+	}()
+	// Return the output channels (read-only) to the caller
+	result := make([]<-chan T, n)
+	for i, ch := range outs {
+		result[i] = ch
+	}
+	return result
+}
+
+// Chunk groups values from the input channel into slices of a given size and sends each chunk to the output channel.
+// The final chunk may be smaller than the chunk size if there are not enough remaining values.
+func Chunk[T any](ctx context.Context, in <-chan T, size int) <-chan []T {
+	out := make(chan []T)
+	go func() {
+		defer close(out)
+		var chunk []T
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				chunk = append(chunk, v)
+				if len(chunk) == size {
+					out <- chunk
+					chunk = nil
 				}
 			}
-		}(i)
-	}
+		}
+		if len(chunk) > 0 {
+			out <- chunk
+		}
+	}()
+	return out
+}
 
+// Window groups incoming values into overlapping windows of a specified size and slide.
+func Window[T any](ctx context.Context, in <-chan T, size int, slide int) <-chan []T {
+	out := make(chan []T)
+	go func() {
+		defer close(out)
+		var window []T
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				window = append(window, v)
+				if len(window) > size {
+					window = window[1:]
+				}
+				if len(window) == size {
+					out <- window
+					window = window[slide:]
+				}
+			}
+		}
+		if len(window) > 0 {
+			out <- window
+		}
+	}()
+	return out
+}
+
+// Distinct removes consecutive duplicate values from the input channel based on a key function.
+func Distinct[T any, K comparable](ctx context.Context, in <-chan T, keyFn func(T) K) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		var last K
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				this := keyFn(v)
+				if this != last {
+					last = this
+					out <- v
+				}
+			}
+		}
+	}()
+	return out
+}
+
+// RateLimiter limits the number of values that can pass through in a given time period buffering up values.
+// if the buffer exceeds `size` the value passing though is dropped.
+func RateLimiter[T any](ctx context.Context, in <-chan T, limit int, per time.Duration, size int) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		ticker := time.NewTicker(per)
+		defer ticker.Stop()
+		var buffer []T
+		var tokens = limit
+	outer:
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				tokens = limit
+			case v, ok := <-in:
+				if !ok {
+					break outer
+				}
+				if tokens > 0 {
+					if len(buffer) > 0 {
+						out <- buffer[0]
+						buffer = buffer[1:]
+						if len(buffer) < size {
+							buffer = append(buffer, v)
+						}
+					} else {
+						out <- v
+					}
+					tokens--
+				} else {
+					if len(buffer) < size {
+						buffer = append(buffer, v)
+					}
+				}
+			}
+		}
+		// drain the buffer
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				tokens = limit
+			default:
+				if tokens > 0 {
+					if len(buffer) > 0 {
+						out <- buffer[0]
+						buffer = buffer[1:]
+						tokens--
+					} else {
+						return
+					}
+				}
+			}
+		}
+	}()
+	return out
+}
+
+// Reduce applies a reducing function to all values from the input channel and produces a single output.
+func Reduce[L, R any](ctx context.Context, in <-chan R, fn func(L, R) L) <-chan L {
+	out := make(chan L)
+	go func() {
+		defer close(out)
+		var acc L
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				acc = fn(acc, v)
+			}
+		}
+		out <- acc
+	}()
 	return out
 }
--- exp/pipeline/pipeline_test.go
+++ exp/pipeline/pipeline_test.go
@@ -2,63 +2,417 @@ package pipeline_test
 
 import (
 	"context"
-	"errors"
-	"runtime"
+	"slices"
+	"sync"
 	"testing"
 	"time"
 
 	. "github.com/cmilhench/x/exp/pipeline"
 )
 
-func Test(t *testing.T) {
-	// create a timeout mechanism
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	go func() {
-		time.Sleep(2 * time.Second)
-		cancel()
-	}()
-
-	n, err := process(ctx, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if n > runtime.NumCPU() {
-		t.Fatalf("expected >%d, actual %d\n", runtime.NumCPU(), n)
-	}
-}
-
-func process(ctx context.Context, numbers []int) (int, error) {
-	workers := runtime.NumCPU()
-	// listen for context cancellation (get a channel)
-	done := ctx.Done()
-
-	// turn our slice or work in to a channel
-	tasks := Generator(done, numbers...)
-	// spin up the workers
-	results := make([]<-chan Result[int], workers)
-	for i := 0; i < workers; i++ {
-		results[i] = Map(ctx, i, tasks, divide)
-	}
-	// receive the answers
-	count := 0
-	errs := []error{}
-	for answer := range FanIn(ctx, results...) {
-		count = count + 1
-
-		if answer.Err != nil {
-			errs = append(errs, answer.Err)
-		}
-	}
-	return count, errors.Join(errs...)
-}
-
-func divide(ctx context.Context, id int, in int) Result[int] {
-	time.Sleep(2000 * time.Millisecond)
-	return Result[int]{Out: 100 / in}
-}
-
 type Result[T any] struct {
 	Out T
 	Err error
+}
+
+func Input(t *testing.T) <-chan int {
+	t.Helper()
+	out := make(chan int)
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	delay := []int{400, 400, 400, 100, 0, 0, 0, 300, 400, 0}
+	go func() {
+		defer close(out)
+		for i, v := range values {
+			time.Sleep(time.Duration(delay[i]) * time.Millisecond)
+			out <- v
+		}
+	}()
+	return out
+}
+
+func AssertExecutionTime(t *testing.T, start time.Time, expected time.Duration, tolerance time.Duration) {
+	t.Helper()
+	elapsed := time.Since(start)
+	if elapsed < expected-tolerance || elapsed > expected+tolerance {
+		t.Errorf("Execution time was %v, expected around %v (±%v)", elapsed, expected, tolerance)
+	}
+}
+
+func AssertResults(t *testing.T, result, expected []int) {
+	t.Helper()
+	if len(result) != len(expected) {
+		t.Fatalf("expected length %d, got %d", len(expected), len(result))
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("at index %d, expected %d, got %d", i, v, result[i])
+		}
+	}
+}
+
+func TestGenerator(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	input := []int{1, 2, 3, 4, 5}
+	output := Generator(ctx, input...)
+
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	if len(result) != len(input) {
+		t.Fatalf("expected length %d, got %d", len(input), len(result))
+	}
+
+	for i, v := range input {
+		if result[i] != v {
+			t.Errorf("at index %d, expected %d, got %d", i, v, result[i])
+		}
+	}
+}
+
+func TestDrop(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Drop(ctx, input, 4)
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{5, 6, 7, 8, 9, 10})
+}
+
+func TestTake(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Take(ctx, input, 5)
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 1300*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{1, 2, 3, 4, 5})
+}
+
+func TestFilter(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Filter(ctx, input, func(val int) bool {
+		return val%2 != 0
+	})
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{1, 3, 5, 7, 9})
+}
+
+func TestMap(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Map(ctx, 0, input, func(ctx context.Context, id int, val int) int {
+		return val * 2
+	})
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20})
+}
+
+func TestFanIn(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input1 := make(chan int)
+	input2 := make(chan int)
+	go func() {
+		defer close(input1)
+		defer close(input2)
+		for i := 0; i < 5; i++ {
+			input1 <- i
+			input2 <- i * 2
+			time.Sleep(200 * time.Millisecond)
+		}
+	}()
+
+	output := FanIn(ctx, input1, input2)
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+	slices.Sort(result)
+
+	AssertExecutionTime(t, start, 1000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{0, 0, 1, 2, 2, 3, 4, 4, 6, 8})
+}
+
+func TestFanOut(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	workerCount := 3
+	var mu sync.Mutex
+	processed := make(map[int]int)
+	fn := func(ctx context.Context, v int) int {
+		mu.Lock()
+		processed[v]++
+		mu.Unlock()
+		time.Sleep(250 * time.Millisecond)
+		return v * 2
+	}
+
+	outputs := FanOut(ctx, input, workerCount, fn)
+	results := make([][]int, workerCount)
+	for i := range outputs {
+		results[i] = make([]int, 0)
+	}
+	wg := sync.WaitGroup{}
+	for i, ch := range outputs {
+		wg.Add(1)
+		go func(index int, c <-chan int) {
+			defer wg.Done()
+			for val := range c {
+				results[index] = append(results[index], val)
+			}
+		}(i, ch)
+	}
+	wg.Wait()
+	var result []int
+	for _, v := range results {
+		result = append(result, v...)
+	}
+	slices.Sort(result)
+
+	AssertExecutionTime(t, start, 2500*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20})
+
+	for i := 1; i <= 10; i++ {
+		if processed[i] != 1 {
+			t.Errorf("expected value %d to be processed once, but got %d times", i, processed[i])
+		}
+	}
+}
+
+func TestThrottle(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Throttle(ctx, input, 100*time.Millisecond)
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 2300*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+}
+
+func TestPartition(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	outputA, outputB := Partition(ctx, 0, input, func(ctx context.Context, id int, val int) bool {
+		return val%2 == 0
+	})
+
+	wg := sync.WaitGroup{}
+	var resultsA []int
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range outputA {
+			resultsA = append(resultsA, v)
+		}
+	}()
+
+	var resultsB []int
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range outputB {
+			resultsB = append(resultsB, v)
+		}
+	}()
+
+	wg.Wait()
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, resultsA, []int{2, 4, 6, 8, 10})
+	AssertResults(t, resultsB, []int{1, 3, 5, 7, 9})
+}
+
+func TestSample(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Sample(ctx, input, 300*time.Millisecond)
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{1, 2, 3, 8, 9})
+}
+
+func TestBroadcast(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	numOutputs := 3
+	outputs := Broadcast(ctx, input, numOutputs)
+	results := make([][]int, numOutputs)
+	for i := range outputs {
+		results[i] = make([]int, 0)
+	}
+	wg := sync.WaitGroup{}
+	for i, ch := range outputs {
+		wg.Add(1)
+		go func(index int, c <-chan int) {
+			defer wg.Done()
+			for val := range c {
+				results[index] = append(results[index], val)
+			}
+		}(i, ch)
+	}
+	wg.Wait()
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 20*time.Millisecond)
+	for _, result := range results {
+		AssertResults(t, result, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	}
+}
+
+func TestChunk(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Chunk(ctx, input, 3)
+	var result [][]int
+	for vals := range output {
+		result = append(result, vals)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result[0], []int{1, 2, 3})
+	AssertResults(t, result[1], []int{4, 5, 6})
+	AssertResults(t, result[2], []int{7, 8, 9})
+	AssertResults(t, result[3], []int{10})
+}
+
+func TestWindow(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Window(ctx, input, 3, 2)
+	var result [][]int
+	for vals := range output {
+		result = append(result, vals)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result[0], []int{1, 2, 3})
+	AssertResults(t, result[1], []int{3, 4, 5})
+	AssertResults(t, result[2], []int{5, 6, 7})
+	AssertResults(t, result[3], []int{7, 8, 9})
+	AssertResults(t, result[4], []int{9, 10})
+}
+
+func TestDistinct(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	input := Generator(ctx, 1, 2, 3, 4, 5, 5, 6, 7, 7, 7, 1, 2, 8, 8, 9, 10, 10)
+
+	output := Distinct(ctx, input, func(val int) int { return val })
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertResults(t, result, []int{1, 2, 3, 4, 5, 6, 7, 1, 2, 8, 9, 10})
+}
+
+func TestRateLimiter(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := RateLimiter(ctx, input, 3, time.Second, 2)
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 3000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{1, 2, 3, 4, 5, 6, 7, 9, 10})
+}
+
+func TestReduce(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	start := time.Now()
+	input := Input(t)
+
+	output := Reduce(ctx, input, func(acc int, val int) int {
+		return acc + val
+	})
+	var result []int
+	for val := range output {
+		result = append(result, val)
+	}
+
+	AssertExecutionTime(t, start, 2000*time.Millisecond, 10*time.Millisecond)
+	AssertResults(t, result, []int{55})
 }
